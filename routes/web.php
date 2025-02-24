d2063b3de61d242ddf6f875fc2a94f3fd369e9c0
Camilo Osorio <brycos15@gmail.com>
2025-02-24 17:44:07 -0500

    Added chat interface

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\File;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    $path = public_path('index.html');
+
+    if (File::exists($path)) {
+        return File::get($path);
+    }
+
+    abort(404);
+})->where('any', '.*');
